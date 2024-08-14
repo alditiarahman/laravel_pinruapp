77ab57148f92e0e57aad7e17b6f31c9bc75b82f7
alditiarahman <117018175+alditiarahman@users.noreply.github.com>
2024-08-14 23:53:51 +0800

    feat : button in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,8 +2,107 @@
     <x-slot name="header">
         {{ __('Dashboard') }}
     </x-slot>
-
-    <div class="p-4 bg-white rounded-lg shadow-xs">
-        {{ __('You are logged in!') }}
+    <div class="p-4 bg-gray-100 rounded-lg shadow-xs">
+        <div class="grid gap-6 mb-1 md:grid-cols-2 xl:grid-cols-4">
+            <!-- Peminjaman Button -->
+            <a href="{{ route('peminjamans.index') }}"
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs black:bg-black-800">
+                <div class="p-3 mr-4 text-purple-500 bg-purple-100 rounded-full dark:text-purple-100 dark:bg-purple-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-lg font-medium text-gray-600 dark:text-gray-600">
+                        Peminjaman
+                    </p>
+                    <p class="text-sm font-semibold text-gray-700 dark:text-gray-700 flex items-center justify-start">
+                        Visit
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6 ml-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </p>
+                </div>
+            </a>
+            <!-- Barang Rusak Button -->
+            <a href="{{ route('barangrusak.index') }}"
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-white">
+                <div
+                    class="p-3 mr-4 text-purple-500 bg-purple-100 rounded-full dark:text-purple-100 dark:bg-purple-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-lg font-medium text-gray-600 dark:text-gray-600">
+                        Barang Rusak
+                    </p>
+                    <p class="text-sm font-semibold text-gray-700 dark:text-gray-700 flex items-center justify-start">
+                        Visit
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6 ml-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </p>
+                </div>
+            </a>
+            <!-- Nilai Ruangan Button -->
+            <a href="{{ route('penilaianruangans.index') }}"
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-white">
+                <div
+                    class="p-3 mr-4 text-purple-500 bg-purple-100 rounded-full dark:text-purple-100 dark:bg-purple-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-lg font-medium text-gray-600 dark:text-gray-600">
+                        Nilai Ruangan
+                    </p>
+                    <p class="text-sm font-semibold text-gray-700 dark:text-gray-700 flex items-center justify-start">
+                        Visit
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6 ml-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </p>
+                </div>
+            </a>
+            <!-- Nilai Petugas Button -->
+            <a href="{{ route('penilaianpetugas.index') }}"
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-white">
+                <div
+                    class="p-3 mr-4 text-purple-500 bg-purple-100 rounded-full dark:text-purple-100 dark:bg-purple-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="mb-2 text-lg font-medium text-gray-600 dark:text-gray-600">
+                        Nilai Petugas
+                    </p>
+                    <p class="text-sm font-semibold text-gray-700 dark:text-gray-700 flex items-center justify-start">
+                        Visit
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6 ml-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </p>
+                </div>
+            </a>
+        </div>
     </div>
 </x-app-layout>
