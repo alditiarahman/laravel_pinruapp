ea13a0ce8db08745a3ec595128265ea2c9eb4223
alditiarahman <117018175+alditiarahman@users.noreply.github.com>
2024-08-15 17:50:27 +0800

    feat : logo login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,9 @@
 <x-guest-layout>
     <div class="flex flex-col overflow-y-auto md:flex-row">
         <div class="h-32 md:h-auto md:w-1/2">
-            <img aria-hidden="true" class="object-cover w-full h-full"
-                 src="{{ asset('images/create-account-office.jpeg') }}" alt="Office"/>
+            <div class="flex justify-center items-center h-full">
+                <img aria-hidden="true" class="w-80 h-w-80" src="{{ asset('images/bawaslu-potrait.png') }}" alt="Office" />
+            </div>
         </div>
 
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
@@ -12,44 +13,30 @@
                 </h1>
 
                 <form method="POST" action="{{ route('register') }}">
-                @csrf
+                    @csrf
 
                     <div class="mt-4">
-                        <x-input-label for="name" :value="__('Name')"/>
-                        <x-text-input type="text"
-                                 id="name"
-                                 name="name"
-                                 class="block w-full"
-                                 value="{{ old('name') }}"
-                                 required
-                                 autofocus/>
+                        <x-input-label for="name" :value="'Name'" />
+                        <x-text-input type="text" id="name" name="name" class="block w-full"
+                            value="{{ old('name') }}" required autofocus />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="email" :value="__('Email')"/>
-                        <x-text-input name="email"
-                                 type="email"
-                                 class="block w-full"
-                                 value="{{ old('email') }}"/>
+                        <x-input-label for="email" :value="'Email'" />
+                        <x-text-input name="email" type="email" class="block w-full" value="{{ old('email') }}" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="password" :value="__('Password')"/>
-                        <x-text-input type="password"
-                                 name="password"
-                                 class="block w-full"
-                                 required/>
+                        <x-input-label for="password" :value="'Password'" />
+                        <x-text-input type="password" name="password" class="block w-full" required />
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label id="password_confirmation" :value="__('Confirm Password')"/>
-                        <x-text-input type="password"
-                                 name="password_confirmation"
-                                 class="block w-full"
-                                 required/>
+                        <x-input-label id="password_confirmation" :value="'Confirm Password'" />
+                        <x-text-input type="password" name="password_confirmation" class="block w-full" required />
                         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     </div>
 
@@ -60,11 +47,11 @@
                     </div>
                 </form>
 
-                <hr class="my-8"/>
+                <hr class="my-4" />
 
                 <p class="mt-4">
-                    <a class="text-sm font-medium text-primary-600 hover:underline"
-                       href="{{ route('login') }}">{{ __('Already registered?') }}</a>
+                    <a class="text-sm font-medium text-primary-600 hover:text-purple-600 hover:underline"
+                        href="{{ route('login') }}">{{ __('Already registered?') }}</a>
                 </p>
             </div>
         </div>
